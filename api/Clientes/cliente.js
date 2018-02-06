86fad2d851a9ad0b8da55ee6688548e6304b9759
Vinicius Cavalcante Computador <contato@viniciuscavalcante.com.br>
2018-02-06 10:17:36 -0200

    Implementação Modulo de Clientes

--- api/Clientes/cliente.js
+++ api/Clientes/cliente.js
@@ -1,9 +1,19 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
-
+required: true
 const clienteSchema = new mongoose.Schema({
-    name: { type: String, required: true},
-    
+    nomeFantasia: { type: String },
+    nomeRazaoSocial: { type: String},
+    endereco: { type: String},
+    numeroEndereco: { type: Number,  min: 0},
+    complemento: { type: String},
+    bairro: { type: String},
+    referencia: { type: String},
+    cidade: { type: String},
+    uf: { type: String},
+    cep: { type: String},
+    telefone: { type: String},
+    email: { type: String}
 },{ versionKey: false})
 
 module.exports = restful.model('Cliente', clienteSchema)
